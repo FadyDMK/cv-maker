da00b24847af5fd0fecb05060dd5242bbdcc581e
fady <32818953+FadyDMK@users.noreply.github.com>
2024-06-17 22:05:12 +0200

    added the ability to add edit and delete new and old education sections

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { faChevronUp, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { v4 as uuidv4 } from "uuid";
 /* eslint react/prop-types: 0 */
 
 export function Education({ educations, setEducations }) {
@@ -11,9 +12,10 @@ export function Education({ educations, setEducations }) {
   );
 }
 
-export function EducationUnit({ educations, setEducations }) {
+function EducationUnit({ educations, setEducations }) {
   // eslint-disable-next-line no-unused-vars
   const [open, setOpen] = useState(false);
+  const [edit, setEdit] = useState(true);
 
   const [education, setEducation] = useState({
     degree: "Bachelor in Physics",
@@ -24,6 +26,54 @@ export function EducationUnit({ educations, setEducations }) {
     endDate: "6/2012",
   });
 
+  const [previewClass, setPreviewClass] = useState("visible");
+  const [editClass, setEditClass] = useState("invisible");
+  function handleEdit(id) {
+    setEdit(true);
+    const edu = educations.find((element) => element.id == id);
+    setEducation(edu);
+    setPreviewClass("invisible");
+    setEditClass("visible");
+  }
+  function handleDelete(id) {
+    setEducations(educations.filter((element) => element.id != id));
+  }
+  function EducationPreview() {
+    return (
+      <div className={previewClass}>
+        {educations.map((element) => (
+          <div key={element.id} className="education-element">
+            <h1>{element.university}</h1>
+            <div className="icons-edu">
+              <FontAwesomeIcon
+                icon={faTrash}
+                onClick={() => {
+                  handleDelete(element.id);
+                }}
+                className="icon"
+              />
+              <FontAwesomeIcon
+                icon={faPen}
+                onClick={() => {
+                  handleEdit(element.id);
+                }}
+                className="icon"
+              />
+            </div>
+          </div>
+        ))}
+        <button className="add-new" onClick={handleNew}>
+          New Education
+        </button>
+      </div>
+    );
+  }
+  function handleNew() {
+    resetEdu();
+    setEdit(false);
+    setPreviewClass("invisible");
+    setEditClass("visible");
+  }
   function handleUniNameChange(e) {
     setEducation({ ...education, university: e.target.value });
   }
@@ -43,6 +93,49 @@ export function EducationUnit({ educations, setEducations }) {
   function handleEndDateChange(e) {
     setEducation({ ...education, endDate: e.target.value });
   }
+
+  function handleCancel() {
+    setPreviewClass("visible");
+    setEditClass("invisible");
+  }
+  function handleClick() {
+    if (edit) {
+      const newEdu = educations.filter((element) => element.id != education.id);
+      const newEducations = [
+        ...newEdu,
+        {
+          degree: education.degree,
+          university: education.university,
+          location: education.location,
+          grade: education.grade,
+          startDate: education.startDate,
+          endDate: education.endDate,
+          id: education.id,
+        },
+      ];
+      setEducations(newEducations);
+    } else {
+      const newEducations = [
+        ...educations,
+        {
+          degree: education.degree,
+          university: education.university,
+          location: education.location,
+          grade: education.grade,
+          startDate: education.startDate,
+          endDate: education.endDate,
+          id: uuidv4(),
+        },
+      ];
+      setEducations(newEducations);
+    }
+    setPreviewClass("visible");
+    setEditClass("invisible");
+  }
+
+  function toggle() {
+    setOpen(!open);
+  }
   function resetEdu() {
     setEducation({
       degree: "",
@@ -52,25 +145,6 @@ export function EducationUnit({ educations, setEducations }) {
       startDate: "",
       endDate: "",
     });
-  }
-  function handleClick() {
-    const newEducations = [
-      ...educations,
-      {
-        degree: education.degree,
-        university: education.university,
-        location: education.location,
-        grade: education.grade,
-        startDate: education.startDate,
-        endDate: education.endDate,
-      },
-    ];
-    setEducations(newEducations);
-    resetEdu();
-  }
-
-  function toggle() {
-    setOpen(!open);
   }
 
   return (
@@ -86,49 +160,55 @@ export function EducationUnit({ educations, setEducations }) {
       </div>
       {open && (
         <>
-          <h1>University Name</h1>
-          <input
-            placeholder="University Name"
-            value={education.university}
-            onChange={handleUniNameChange}
-          />
-          <h1>Degree</h1>
-          <input
-            placeholder="Degree Name"
-            value={education.degree}
-            onChange={handleDegreeChange}
-          />
-          <h1>Location</h1>
-          <input
-            placeholder="Location"
-            value={education.location}
-            onChange={handleLocationChange}
-          />
-          <h1>Grade</h1>
-          <input
-            placeholder="Grade"
-            value={education.grade}
-            onChange={handleGradeChange}
-          />
-          <h1>Start Date</h1>
-          <input
-            placeholder="Start Date"
-            value={education.startDate}
-            onChange={handleStartDateChange}
-          />
-          <h1>End Date</h1>
-          <input
-            placeholder="End Date"
-            value={education.endDate}
-            onChange={handleEndDateChange}
-          />
-          <div className="btns">
-            <button onClick={handleClick} className="add-btn">
-              Add
-            </button>
-            <button onClick={resetEdu} className="reset-btn">
-              Reset
-            </button>
+          <EducationPreview educations={educations} />
+          <div className={editClass}>
+            <h1>University Name</h1>
+            <input
+              placeholder="University Name"
+              value={education.university}
+              onChange={handleUniNameChange}
+            />
+            <h1>Degree</h1>
+            <input
+              placeholder="Degree Name"
+              value={education.degree}
+              onChange={handleDegreeChange}
+            />
+            <h1>Location</h1>
+            <input
+              placeholder="Location"
+              value={education.location}
+              onChange={handleLocationChange}
+            />
+            <h1>Grade</h1>
+            <input
+              placeholder="Grade"
+              value={education.grade}
+              onChange={handleGradeChange}
+            />
+            <h1>Start Date</h1>
+            <input
+              placeholder="Start Date"
+              value={education.startDate}
+              onChange={handleStartDateChange}
+            />
+            <h1>End Date</h1>
+            <input
+              placeholder="End Date"
+              value={education.endDate}
+              onChange={handleEndDateChange}
+            />
+            <div className="btns">
+              <button onClick={handleClick} className="add-btn">
+                {edit ? "Edit" : "Add"}
+              </button>
+              <button onClick={handleCancel} className="cancel-btn">
+                Cancel
+              </button>
+              <button onClick={resetEdu} className="reset-btn">
+                Reset
+              </button>
+            </div>
           </div>
         </>
       )}
--- src/components/example-info.js
+++ src/components/example-info.js
@@ -3,25 +3,37 @@ export const exampleInfo = {
     fullName: "Rick Sanchez",
     email: "rick.smart@galactic.com",
     phoneNumber: "+36 21 157 125",
-    location:'Earth',
+    location: "Earth",
   },
-  education: [{
-    degree: "Bachelor in Physics",
-    university: "Gluglug Intergalactic University",
-    location: "Andromeda Galaxy",
-    grade: "3.99/4",
-    startDate: "5/2010",
-    endDate: "6/2012",
-  }],
+  education: [
+    {
+      id:"1",
+      degree: "pp",
+      university: "pipipopo",
+      location: "Andromeda Galaxy",
+      grade: "3.99/4",
+      startDate: "5/2010",
+      endDate: "6/2012",
+    },
+    {
+      id:"2",
+      degree: "Bachelor in Physics",
+      university: "Gluglug Intergalactic University",
+      location: "Andromeda Galaxy",
+      grade: "3.99/4",
+      startDate: "5/2010",
+      endDate: "6/2012",
+    },
+  ],
   work: [
     {
       position: "Scientist and Inventor",
       company: "SpaceX",
       location: "Florida, US",
-      description: "Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.",
+      description:
+        "Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.",
       startDate: "1/2024",
       endDate: "present",
-
-    }
+    },
   ],
 };

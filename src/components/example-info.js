8880182227c24d5463f2aa173bf57b32b880aeaa
fady <32818953+FadyDMK@users.noreply.github.com>
2024-06-18 12:30:52 +0200

    added exemple

--- src/components/example-info.js
+++ src/components/example-info.js
@@ -1,43 +1,3 @@
-// export const exampleInfo = {
-//   personalInfo: {
-//     fullName: "Rick Sanchez",
-//     email: "rick.smart@galactic.com",
-//     phoneNumber: "+36 21 157 125",
-//     location: "Earth",
-//   },
-//   education: [
-//     {
-//       id:"1",
-//       degree: "pp",
-//       university: "pipipopo",
-//       location: "Andromeda Galaxy",
-//       grade: "3.99/4",
-//       startDate: "5/2010",
-//       endDate: "6/2012",
-//     },
-//     {
-//       id:"2",
-//       degree: "Bachelor in Physics",
-//       university: "Gluglug Intergalactic University",
-//       location: "Andromeda Galaxy",
-//       grade: "3.99/4",
-//       startDate: "5/2010",
-//       endDate: "6/2012",
-//     },
-//   ],
-//   work: [
-//     {
-//       position: "Scientist and Inventor",
-//       company: "SpaceX",
-//       location: "Florida, US",
-//       description:
-//         "Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.",
-//       startDate: "1/2024",
-//       endDate: "present",
-//       id:"1"
-//     },
-//   ],
-// };
 export const exampleInfo = {
   personalInfo: {
     fullName: "Rick Sanchez",
@@ -45,6 +5,46 @@ export const exampleInfo = {
     phoneNumber: "+36 21 157 125",
     location: "Earth",
   },
-  education: [],
-  work: [],
+  education: [
+    {
+      id:"1",
+      degree: "pp",
+      university: "pipipopo",
+      location: "Andromeda Galaxy",
+      grade: "3.99/4",
+      startDate: "5/2010",
+      endDate: "6/2012",
+    },
+    {
+      id:"2",
+      degree: "Bachelor in Physics",
+      university: "Gluglug Intergalactic University",
+      location: "Andromeda Galaxy",
+      grade: "3.99/4",
+      startDate: "5/2010",
+      endDate: "6/2012",
+    },
+  ],
+  work: [
+    {
+      position: "Scientist and Inventor",
+      company: "SpaceX",
+      location: "Florida, US",
+      description:
+        "Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.",
+      startDate: "1/2024",
+      endDate: "present",
+      id:"1"
+    },
+  ],
 };
+// export const exampleInfo = {
+//   personalInfo: {
+//     fullName: "Rick Sanchez",
+//     email: "rick.smart@galactic.com",
+//     phoneNumber: "+36 21 157 125",
+//     location: "Earth",
+//   },
+//   education: [],
+//   work: [],
+// };
